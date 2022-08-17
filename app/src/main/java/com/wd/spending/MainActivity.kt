2a9bfff471a1f4908ce544a72d6f09071846914c
Walter Shih <left.16@gmail.com>
2022-08-18 03:25:09 +0800

    feat(Function): grant permission and setup auth flow

--- app/src/main/java/com/wd/spending/MainActivity.kt
+++ app/src/main/java/com/wd/spending/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.wd.spending
 
+import android.Manifest
+import android.accounts.AccountManager
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -14,17 +18,30 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
-import com.google.api.services.sheets.v4.SheetsScopes
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import com.wd.spending.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val homeViewModel: HomeViewModel by viewModels()
+
+    @Inject
+    lateinit var googleCredential: GoogleAccountCredential
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -36,15 +53,40 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
-        setupActionBarWithNavController (navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        initPermission()
+    }
+
+    private fun initPermission() {
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.GET_ACCOUNTS)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    // Start a dialog from which the user can choose an account
+                    startActivityForResult(
+                        googleCredential.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER
+                    )
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    Timber.d("onPermissionDenied")
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    Timber.d("onPermissionRationaleShouldBeShown")
+                }
+            }).check()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -56,5 +98,30 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQUEST_ACCOUNT_PICKER -> {
+                if (resultCode == RESULT_OK) {
+                    val accountName = data!!.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
+                    if (accountName != null) {
+                        googleCredential.selectedAccountName = accountName
+                    }
+
+                    homeViewModel.getPaymentAndSpend()
+                }
+            }
+            REQUEST_AUTHORIZATION -> {
+                if (resultCode == RESULT_OK) {
+                    Timber.d("auth ok")
+                    homeViewModel.getPaymentAndSpend()
+                } else {
+                    Timber.d("auth fail")
+                }
+            }
+        }
     }
 }
