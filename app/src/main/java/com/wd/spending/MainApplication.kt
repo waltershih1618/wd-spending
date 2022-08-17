c112e8e67981bd323332f9767033629861524769
Walter Shih <left.16@gmail.com>
2022-08-18 02:55:57 +0800

    feat(Setting): add dependency & permission
    - add timber & dexter

--- app/src/main/java/com/wd/spending/MainApplication.kt
+++ app/src/main/java/com/wd/spending/MainApplication.kt
@@ -2,8 +2,13 @@ package com.wd.spending
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
-class MainApplication: Application() {
+class MainApplication : Application() {
 
+    init {
+        Timber.plant(DebugTree())
+    }
 }
